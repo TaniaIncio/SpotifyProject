714096c45cf1206adca1befe992f7abfdd825ff3
GMDTest <mazziel1993@gmail.com>
2017-03-03 10:16:01 -0500

    Maquetación de lista de canciones.

---
+++ app/src/main/java/com/tincio/spotifyproject/presentation/activity/ListaCancionesActivity.java
@@ -0,0 +1,14 @@
+package com.tincio.spotifyproject.presentation.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.tincio.spotifyproject.R;
+
+public class ListaCancionesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_canciones);
+    }
+}
